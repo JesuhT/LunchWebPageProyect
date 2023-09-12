b6af9c7326410257d049ef3d9d6d9c5478fd787d
JesuhT <eliseotorres1798@gmail.com>
2023-09-12 10:59:45 -0500

    prueba otro pc

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" ></script>
+$(document).ready(function(){
+    $(".mobile-nav i").click(function(){
+        $(".nav").toggleClass("mobile-menu");
+    });
+});
