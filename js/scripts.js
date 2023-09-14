e49e755149c58cfe5b4763a9c6f3fe4e75db4c11
JesuhT <eliseotorres1798@gmail.com>
2023-09-14 13:42:21 -0500

    borre innecesario

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,36 @@
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" ></script>
-$(document).ready(function(){
-    $(".mobile-nav i").click(function(){
-        $(".nav").toggleClass("mobile-menu");
-    });
-});
+
+
+//Script normal
+var list = document.querySelector('.slider .slider-items');
+var items = document.querySelectorAll('.items');
+var dots = document.querySelectorAll('.slider .dots li');
+var prev = document.getElementById('prev');
+var next = document.getElementById('next');
+
+var active = 0;
+let checkleft;
+next.onclick = function () {
+    if (active + 1 > items.length - 1) {
+        active = 0;
+    }
+    else {
+        active = active + 1;
+    }
+    reloadSlider();
+}
+function reloadSlider() {
+    checkleft = items[active].offsetLeft;
+    if (checkleft - 300 <130) {
+        checkleft += 300;
+        console.log(checkleft);
+        list.style.left = -checkleft + 'px';
+    }
+    else {
+        console.log(checkleft);
+        console.log("este");
+        list.style.left = -checkleft + 'px';
+    }
+
+
+}
