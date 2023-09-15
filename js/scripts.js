ba4c79b4cea298318d2e89ad69ddd80f859d08e2
JesuhT <eliseotorres1798@gmail.com>
2023-09-15 06:28:24 -0500

    penultimos cambios

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,16 @@ $(document).ready(function(){
 });
 
 
-//Script normal
-var list = document.querySelector('.slider .slider-items');
-var items = document.querySelectorAll('.items');
-var dots = document.querySelectorAll('.slider .dots li');
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
-
-var active = 0;
-let checkleft;
-next.onclick = function () {
-    if (active + 1 > items.length - 1) {
-        active = 0;
-    }
-    else {
-        active = active + 1;
-    }
-    reloadSlider();
-}
-function reloadSlider() {
-    checkleft = items[active].offsetLeft;
-    if (checkleft - 300 <130) {
-        checkleft += 300;
-        console.log(checkleft);
-        list.style.left = -checkleft + 'px';
-    }
-    else {
-        console.log(checkleft);
-        console.log("este");
-        list.style.left = -checkleft + 'px';
-    }
-
-
-}
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
